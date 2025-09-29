56815329d4c6271d9ac163258d4045db759ce883
zhom0624 <samenelya@gmail.com>
2025-09-29 22:19:12 +0500

    Fixed checks

--- src/main/java/org/qubership/integration/platform/runtime/catalog/service/exportimport/migrations/chain/V104ChainImportFileMigration.java
+++ src/main/java/org/qubership/integration/platform/runtime/catalog/service/exportimport/migrations/chain/V104ChainImportFileMigration.java
@@ -1,8 +1,8 @@
 package org.qubership.integration.platform.runtime.catalog.service.exportimport.migrations.chain;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.stereotype.Component;
 
