b66af94a20a1bf25786eec232a960086d794dbc6
zhonniks <samenelya@gmail.com>
2025-10-20 11:21:48 +0500

    Updated pom

--- src/main/java/org/qubership/integration/platform/runtime/catalog/service/exportimport/migrations/chain/V104ChainImportFileMigration.java
+++ src/main/java/org/qubership/integration/platform/runtime/catalog/service/exportimport/migrations/chain/V104ChainImportFileMigration.java
@@ -4,10 +4,12 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import static org.qubership.integration.platform.runtime.catalog.service.exportimport.ExportImportConstants.*;
 
+@Slf4j
 @Component
 public class V104ChainImportFileMigration implements ChainImportFileMigration {
     private final YAMLMapper yamlMapper = new YAMLMapper();
@@ -19,6 +21,7 @@ public class V104ChainImportFileMigration implements ChainImportFileMigration {
 
     @Override
     public ObjectNode makeMigration(ObjectNode fileNode) throws JsonProcessingException {
+        log.debug("Applying chain migration: {}", getVersion());
         processElementsAction(fileNode.get(ELEMENTS));
 
         return fileNode;
@@ -34,17 +37,17 @@ public class V104ChainImportFileMigration implements ChainImportFileMigration {
             JsonNode elemType = element.get(ELEMENT_TYPE);
             ObjectNode properties = (ObjectNode) element.get(PROPERTIES);
 
-            if (
-               elemType == null || !"http-trigger".equals(elemType.asText())
-               || !properties.has("accessControlType") && !"ABAC".equals(properties.get("accessControlType").asText())
-            ) {
+            if (elemType == null || !"http-trigger".equals(elemType.asText())) {
                 continue;
             }
 
-            ObjectNode abacParameters = (ObjectNode) properties.get("abacParameters");
-            if (abacParameters == null || abacParameters.isNull()) {
-                abacParameters = yamlMapper.createObjectNode();
+            if (properties == null || !properties.has("accessControlType") || !"ABAC".equals(properties.get("accessControlType").asText())) {
+                continue;
             }
+
+            ObjectNode abacParameters = properties.has("abacParameters") && properties.get("abacParameters").isObject()
+                    ? (ObjectNode) properties.get("abacParameters")
+                    : yamlMapper.createObjectNode();
 
             if (!abacParameters.has("resourceType")) {
                 abacParameters.put("resourceType", "CHAIN");
