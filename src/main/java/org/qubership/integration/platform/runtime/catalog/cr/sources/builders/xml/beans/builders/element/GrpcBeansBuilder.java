54b101ccb698b36a7569656d88666d6c63b9ce63
Sergei Skuratovich <900852+SSNikolaevich@users.noreply.github.com>
2025-11-19 12:25:18 +0300

    feat: added beans binders

---
+++ src/main/java/org/qubership/integration/platform/runtime/catalog/cr/sources/builders/xml/beans/builders/element/GrpcBeansBuilder.java
@@ -0,0 +1,49 @@
+package org.qubership.integration.platform.runtime.catalog.cr.sources.builders.xml.beans.builders.element;
+
+import org.codehaus.stax2.XMLStreamWriter2;
+import org.qubership.integration.platform.runtime.catalog.cr.sources.SourceBuilderContext;
+import org.qubership.integration.platform.runtime.catalog.cr.sources.builders.xml.beans.ElementBeansBuilder;
+import org.qubership.integration.platform.runtime.catalog.persistence.configs.entity.chain.element.ChainElement;
+import org.springframework.stereotype.Component;
+
+import static org.qubership.integration.platform.runtime.catalog.model.constant.CamelNames.*;
+
+@Component
+public class GrpcBeansBuilder implements ElementBeansBuilder {
+    @Override
+    public boolean applicableTo(ChainElement element) {
+        return SERVICE_CALL_COMPONENT.equals(element.getType())
+                && OPERATION_PROTOCOL_TYPE_GRPC.equals(
+                        element.getPropertyAsString(OPERATION_PROTOCOL_TYPE_PROP));
+    }
+
+    @Override
+    public void build(XMLStreamWriter2 streamWriter, ChainElement element, SourceBuilderContext context) throws Exception {
+        streamWriter.writeStartElement("bean");
+        streamWriter.writeAttribute("name", element.getId());
+        streamWriter.writeAttribute("type", "io.micrometer.core.instrument.binder.grpc.MetricCollectingClientInterceptor");
+        streamWriter.writeAttribute("builderClass", "org.qubership.integration.platform.engine.util.builders.GrpcMetricCollectingClientInspectorBuilder");
+        streamWriter.writeAttribute("builderMethod", "build");
+
+        streamWriter.writeStartElement("properties");
+
+        streamWriter.writeEmptyElement("property");
+        streamWriter.writeAttribute("key", "chainId");
+        streamWriter.writeAttribute("value", element.getChain().getId());
+
+        streamWriter.writeEmptyElement("property");
+        streamWriter.writeAttribute("key", "chainName");
+        streamWriter.writeAttribute("value", element.getChain().getName());
+
+        streamWriter.writeEmptyElement("property");
+        streamWriter.writeAttribute("key", "elementId");
+        streamWriter.writeAttribute("value", element.getId());
+
+        streamWriter.writeEmptyElement("property");
+        streamWriter.writeAttribute("key", "elementName");
+        streamWriter.writeAttribute("value", element.getName());
+
+        streamWriter.writeEndElement();
+        streamWriter.writeEndElement();
+    }
+}
