1bf56b0ed1068de3a8f2f7e4124e5590786604c0
AkhDmitrii <fuzzzi2410@gmail.com>
2026-02-18 16:46:44 +0300

    fix: get environment address instead of service name

--- src/main/java/org/qubership/integration/platform/runtime/catalog/service/EnvironmentService.java
+++ src/main/java/org/qubership/integration/platform/runtime/catalog/service/EnvironmentService.java
@@ -123,9 +123,4 @@ public class EnvironmentService extends EnvironmentBaseService {
         }
         return result;
     }
-
-    public Environment getByIdForSystemOrElseNull(String systemId, String environmentId) {
-        return environmentRepository.findBySystemIdAndId(systemId, environmentId)
-                .orElse(null);
-    }
 }
