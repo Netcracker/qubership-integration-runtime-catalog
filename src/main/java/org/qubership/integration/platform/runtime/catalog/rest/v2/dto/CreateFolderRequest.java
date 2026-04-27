659a040bf86bdae4a079125fefc8414df4e9a38a
ravikant31 <ravikant.ceg@gmail.com>
2026-04-27 12:26:51 +0530

    refactor(v2): extract AbstractFolderWriteRequest for folder create/update DTOs
    - Move shared parentId, name, description and Bean Validation into
      AbstractFolderWriteRequest.
    - CreateFolderRequest keeps optional id only.
    - Fix UpdateFolderRequest @Schema description (was Create folder request object).
    - Fix done to pass SonarQube Quality Gate

--- src/main/java/org/qubership/integration/platform/runtime/catalog/rest/v2/dto/CreateFolderRequest.java
+++ src/main/java/org/qubership/integration/platform/runtime/catalog/rest/v2/dto/CreateFolderRequest.java
@@ -1,8 +1,6 @@
 package org.qubership.integration.platform.runtime.catalog.rest.v2.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,20 +11,8 @@ import lombok.experimental.SuperBuilder;
 @SuperBuilder
 @NoArgsConstructor
 @Schema(description = "Create folder request object")
-public class CreateFolderRequest {
+public class CreateFolderRequest extends AbstractFolderWriteRequest {
+
     @Schema(description = "Folder ID")
     String id;
-
-    @Schema(description = "Parent folder ID")
-    @Size(max = 255)
-    String parentId;
-
-    @Schema(description = "Name")
-    @NotBlank
-    @Size(max = 255)
-    String name;
-
-    @Schema(description = "Description")
-    @Size(max = 255)
-    String description;
 }
--- src/main/java/org/qubership/integration/platform/runtime/catalog/rest/v2/dto/UpdateFolderRequest.java
+++ src/main/java/org/qubership/integration/platform/runtime/catalog/rest/v2/dto/UpdateFolderRequest.java
@@ -1,8 +1,6 @@
 package org.qubership.integration.platform.runtime.catalog.rest.v2.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -12,18 +10,6 @@ import lombok.experimental.SuperBuilder;
 @Setter
 @SuperBuilder
 @NoArgsConstructor
-@Schema(description = "Create folder request object")
-public class UpdateFolderRequest {
-    @Schema(description = "Parent folder ID")
-    @Size(max = 255)
-    String parentId;
-
-    @Schema(description = "Name")
-    @NotBlank
-    @Size(max = 255)
-    String name;
-
-    @Schema(description = "Description")
-    @Size(max = 255)
-    String description;
+@Schema(description = "Update folder request object")
+public class UpdateFolderRequest extends AbstractFolderWriteRequest {
 }
